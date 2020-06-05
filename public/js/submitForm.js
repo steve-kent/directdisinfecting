a8fe891de733684c3ecb7251acbf397c74397160
steve-kent <39534870+steve-kent@users.noreply.github.com>
2020-06-05 15:32:23 -0400

    Get all form field values when submitting

--- public/js/submitForm.js
+++ public/js/submitForm.js
@@ -2,15 +2,25 @@ function submitForm()
 {
     console.log("Submit button clicked");
 
-    const email = document.getElementById('email').nodeValue;
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+    const footage = document.getElementById('footage').value;
+    const facility = document.getElementById('facility').value;
 
     axios.post('https://us-central1-direct-disinfecting.cloudfunctions.net/submit', 
     {
-        email: email
+        name: name,
+        email: email,
+        phone: phone,
+        message: message,
+        footage: footage,
+        facility: facility
     })
     .then(function(response) 
     {
-        console.log('response: ' + response);
+        console.log('response: ' + JSON.stringify(response));
     })
     .catch(function(error) 
     {
